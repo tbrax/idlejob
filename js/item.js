a13de4232a5126a7df4c408d591ad1d91c601dff
tbrax <taakbrax@gmail.com>
2023-06-12 19:10:18 -0800

    item class

---
+++ js/item.js
@@ -0,0 +1,9 @@
+export class Item {
+    constructor(name) {
+      this.name = name;
+    }
+
+    getName() {
+        return this.name
+    }
+  }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 
+import {Item} from '/item.js';
+
+
 function main () {
-    console.log('Script is working')
+
+    const i = Item('Test SCript')
+
+    console.log(i.getName())
     document.getElementById("demo").innerHTML = "Test Script";
 }
 
