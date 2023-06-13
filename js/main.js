22e7f4c35416b8c42df64b958616beea91246fb0
tbrax <taakbrax@gmail.com>
2023-06-12 18:50:54 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-document.getElementById("demo").innerHTML = "Test Script";
+
+function main () {
+    console.log('Script is working')
+    document.getElementById("demo").innerHTML = "Test Script";
+}
+
+main();
